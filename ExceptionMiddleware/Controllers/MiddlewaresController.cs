7e98e3d5f96a3dcf07e91fa2410ea5376434369e
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-05-08 16:59:19 +0300

    Add project files.

---
+++ ExceptionMiddleware/Controllers/MiddlewaresController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionMiddleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MiddlewaresController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public string Get()
+        {
+            
+                throw new Exception("Test error");
+            
+          
+            return "OK";
+        }
+
+    }
+}
---
+++ ExceptionMiddleware/Controllers/TestsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExceptionMiddleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            int a = 0;
+            int b = 10 / a;
+            return "Ok";
+        }
+    }
+}
